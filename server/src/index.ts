256edebdf618a7f6dfda29346a3b2546c6522824
Алекандр Авдеев <alexander.avdeev@balance-pl.ru>
2022-02-12 14:23:01 +0300

    add prettier and eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+
+  const ogpdgopdfogpdfogpdfogpdfogpdfgopdfgodpfgopdfg = 10;
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
 });
